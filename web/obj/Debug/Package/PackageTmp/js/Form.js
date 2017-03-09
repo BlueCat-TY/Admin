139eb78bbe31551a4fe6398878322825be79d08d
songxuan <974914313@qq.com>
2017-03-09 17:51:33 +0800

    update

--- web/obj/Debug/Package/PackageTmp/js/Form.js
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="jquery/jquery-3.1.1.min.js" />
-
-(function ($) {
-    $.fn.labelauty = function (tag, tag2) {
-
-        alert($(this).prop("outerHTML"));
-        
-    }
-}(jQuery));
